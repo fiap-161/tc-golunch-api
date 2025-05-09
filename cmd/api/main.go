2d7f92ad65660e39f5f9323e4db5324965792755
Amanda <mandacosta94@gmail.com>
2025-05-09 08:16:46 -0300

    Base para o swago

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	_ "github.com/fiap-161/tech-challenge-fiap161/docs"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title           GoLunch
+// @version         1.0
+// @description     Rest API para facilitar o gerenciamento de pedidos em uma lanchonete
+// @host            localhost:8080
+// @BasePath        /
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", ping)
 
+	// use ginSwagger middleware to serve the API docs
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run()
+}
+
+// Ping godoc
+// @Summary      Responde com "Pong"
+// @Description  Health Check
+// @Tags         Ping
+// @Accept       json
+// @Produce      json
+// @Success      200 {object}  PongResponse
+// @Router       /ping [get]
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+type PongResponse struct {
+	Message string `json:"message" example:"pong"`
 }
